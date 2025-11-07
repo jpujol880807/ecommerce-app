914ee1fbff777e163e39746c2b7e8fee9c02c3cc
Julio <jpujol880807@gmail.com>
2025-11-07 16:51:54 +0000

    Add auth and deploy script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,36 @@
 // nuxt.config.ts
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
-  build: {
-    transpile: ['vuetify'], // Ensures Vuetify is transpiled correctly
-  },
-  css: [
-    'vuetify/styles', // Already imported in the plugin, but good to have here too if needed
-    '@mdi/font/css/materialdesignicons.css',
-  ],
-  modules: [// This is the recommended way to use vite-plugin-vuetify with Nuxt 3/4
-  async (_options, nuxt) => {
-    nuxt.hooks.hook('vite:extendConfig', (config) => {
-      if (config.plugins) {
-        config.plugins.push(
-            vuetify({
-              autoImport: true
-            })
-        );
-      }
-    });
-  }, '@pinia/nuxt'],
-  // You no longer need to explicitly add the plugin to a 'plugins' array in nuxt.config.ts,
-  // as Nuxt 4 automatically registers files in the plugins directory.
+    build: {
+        transpile: ['vuetify'],
+    },
+    css: [
+        'vuetify/styles',
+        '@mdi/font/css/materialdesignicons.css',
+    ],
+    modules: [
+        async (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                if (config.plugins) {
+                    config.plugins.push(
+                        vuetify({
+                            autoImport: true
+                        })
+                    );
+                }
+            });
+        },
+        '@pinia/nuxt',
+        'nuxt-auth-utils'
+    ],
+    runtimeConfig: {
+        session: {
+            password: '',
+            name: 'ecommerce_session',
+            cookie: {
+                maxAge:  60 * 24 * 7
+            }
+        }
+    },
 });
