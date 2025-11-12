f601841966cc6576f856ba09a2e2f07cfcee743e
Julio <jpujol880807@gmail.com>
2025-11-12 22:34:42 +0000

    add drizzle config for turso migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,32 @@
-import {defineConfig} from 'drizzle-kit';
+import {Config} from 'drizzle-kit';
 import 'dotenv/config';
 
-const credentials = process.env.NODE_ENV === 'production' ? {
+const isProduction = process.env.NODE_ENV === 'production';
+const credentials = isProduction ? {
     url: process.env.TURSO_DATABASE_URL as string,
     authToken: process.env.TURSO_DATABASE_AUTH_TOKEN as string,
 } : {
-    url: 'file:database/sqlite/database.sqlite',
+    url: 'file:database/sqlite/database.sqlite'
 };
-
-export default defineConfig({
+const localConfig = {
     dialect: 'sqlite',
     out: './common/infrastructure/db/drizzle/migrations',
     schema: './common/infrastructure/db/drizzle/schema.ts',
-    dbCredentials: credentials
-})
+    dbCredentials: {
+        url: 'file:database/sqlite/database.sqlite'
+    }
+} satisfies Config;
+
+const productionConfig = {
+    dialect: 'turso',
+    out: './common/infrastructure/db/drizzle/migrations',
+    schema: './common/infrastructure/db/drizzle/schema.ts',
+    dbCredentials: {
+        url: process.env.TURSO_DATABASE_URL as string,
+        authToken: process.env.TURSO_DATABASE_AUTH_TOKEN as string,
+    }
+} satisfies Config;
+
+const config = isProduction ? productionConfig : localConfig;
+export default config;
+
